e620c8c535b0920ec57a436c6e697c95e74b7821
NzamaE <nzamaernest1@gmail.com>
2025-07-28 21:26:00 -0400

    on task 7

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,22 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -50,16 +64,47 @@ public_users.get('/author/:author', function (req, res) {
   });
   
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  public_users.get('/title/:title', function (req, res) {
+    const rawTitle = req.params.title;
+    const finalTitle = rawTitle.split("-").join(" "); // e.g. 'The-Divine-Comedy' => 'The Divine Comedy'
+  
+    const matchingBooks = [];
+  
+    for (let key in books) {
+
+      if (books[key].title === finalTitle) {
+        matchingBooks.push(books[key]);
+
+
+
+      }
+    }
+  
+    if (matchingBooks.length > 0) {
+
+      return res.json(matchingBooks);
+
+    } else {
+
+      return res.status(404).json({ message: "Title not found" });
+    }
+  });
+  
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+
+  const book = books[isbn];
+  
+  if (book) {
+
+    return res.json(book.reviews);
+
+  } else {
+
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
