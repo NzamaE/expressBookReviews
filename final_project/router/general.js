661655cf055a835e5b871da29f7a283172871daf
NzamaE <nzamaernest1@gmail.com>
2025-07-28 20:49:33 -0400

    Get Book by author complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,10 +26,29 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const rawAuthor = req.params.author;
+    console.log("Received:", rawAuthor);
+    
+    // Replace hyphens with spaces (works better for multi-word names)
+    const finalAuthor = rawAuthor.split("-").join(" ");
+    console.log("Final Author String:", finalAuthor);
+    
+    // Find books that match this author
+    const matchingBooks = [];
+    for (let key in books) {
+      if (books[key].author === finalAuthor) {
+        matchingBooks.push(books[key]);
+      }
+    }
+  
+    if (matchingBooks.length > 0) {
+      res.send(matchingBooks);
+    } else {
+      res.status(404).send({ message: "Author not found" });
+    }
+  });
+  
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
